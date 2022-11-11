9084e1603efe79376c06f68701efbe7b2609db1b
ZltZZZ <93077810+ZltZZZ@users.noreply.github.com>
2022-11-11 06:28:34 +0300

    Add engine_send_command() and the same other func.
    Added functions for the interaction of the application with the engine process. Added processing and sending of some standard messages.

--- src/Engine_handler.cpp
+++ src/Engine_handler.cpp
@@ -11,6 +11,18 @@ void init_engine_struct(engine* engn, LPCWSTR path_to_engine) {
 engine_error engine_load(engine* engn) {
 	// Creates an engine process (run engine.exe)
 	if (create_process(engn->path_to_engine, &engn->pipe_in_w, &engn->pipe_out_r) == PROCESS_CREATE_OK) {
+		char answ[MAX_MSG_SIZE] = { '\0' };
+
+		// Wait for initializing of engine (synchronize engine and app) (wait answ)
+		wait_for_answ(&engn->pipe_out_r);
+		clean_pipe_out(&engn->pipe_out_r);
+
+		// Send command "uci" as specificated in uci protocol docs
+		engine_send_command(engn, COMMAND_TYPE_GET_ANSW, UCI, answ);
+		if (engine_identify_answer(answ) != UCI_OK) {
+			return ENGINE_LOAD_FAIL;
+		}
+
 		engn->state = WORKING;
 		return ENGINE_LOAD_OK;
 	}
@@ -20,8 +32,79 @@ engine_error engine_load(engine* engn) {
 }
 
 void engine_close(engine* engn) {
-	//òóò äîáàâèòü îòïðàâêó êîìàíäû äâèæêó çàâåðøèòü ðàáîòó (quit)
+	engine_send_command(engn, COMMAND_TYPE_PASSTHROUGH, QUIT_ENGINE, NULL);
 	close_stream_handle(&engn->pipe_in_w);
 	close_stream_handle(&engn->pipe_out_r);
 	engn->state = NOT_LOADED;
+}
+
+/* Send command to engine. If command type set as COMMAND_TYPE_PASSTHROUGH, than buff_answ should be set as NULL*/
+void engine_send_command(engine* engn, engine_command_type command_type, engine_command command, char* buff_answ) {
+	char msg[MAX_MSG_SIZE] = { '\0' };
+
+	switch (command)
+	{
+	case UCI:
+		memcpy(msg, "uci\n", sizeof(char) * 5);
+		break;
+	case DEBUG_ON:
+		break;
+	case DEBUG_OFF:
+		break;
+	case IS_READY:
+		memcpy(msg, "isready\n", sizeof(char) * 9);
+		break;
+	case SET_OPTION:
+		break;
+	case UCI_NEW_GAME:
+		memcpy(msg, "ucinewgame\n", sizeof(char) * 12);
+		break;
+	case DO_NEXT_MOVE:
+		break;
+	case START_ANALYZE:
+		memcpy(msg, "go\n", sizeof(char) * 4);
+		break;
+	case STOP_ANALYZE:
+		memcpy(msg, "stop\n", sizeof(char) * 6);
+		break;
+	case QUIT_ENGINE:
+		memcpy(msg, "quit\n", sizeof(char) * 6);
+		break;
+	default:
+		break;
+	}
+
+	switch (command_type)
+	{
+	case COMMAND_TYPE_PASSTHROUGH:
+		send_message(&engn->pipe_in_w, msg);
+		break;
+	case COMMAND_TYPE_GET_ANSW:
+		send_message(&engn->pipe_in_w, msg);
+		wait_for_answ(&engn->pipe_out_r);
+		recieve_message(&engn->pipe_out_r, buff_answ);
+		break;
+	default:
+		break;
+	}
+}
+
+void engine_set_options() {
+
+}
+
+engine_answers engine_identify_answer(char* answ) {
+	if (answ[0] == '\0') {
+		return NO_ANSW;
+	}
+	else if (strstr(answ, "uciok") != NULL)
+	{
+		return UCI_OK;
+	}
+	else if (strstr(answ, "readyok") != NULL) {
+		return READY_OK;
+	}
+	else {
+		return NORMAL_ANSW;
+	}
 }
--- src/Engine_handler.h
+++ src/Engine_handler.h
@@ -11,6 +11,31 @@ typedef enum _engine_error {
 	ENGINE_LOAD_FAIL
 } engine_error;
 
+typedef enum _engine_command_type {
+	COMMAND_TYPE_PASSTHROUGH, // Send command without response
+	COMMAND_TYPE_GET_ANSW     // Send command and wait for answer
+} engine_command_type;
+
+typedef enum _engine_command {
+	UCI,
+	DEBUG_ON,
+	DEBUG_OFF,
+	IS_READY,
+	SET_OPTION,
+	UCI_NEW_GAME,
+	DO_NEXT_MOVE,
+	START_ANALYZE,
+	STOP_ANALYZE,
+	QUIT_ENGINE
+} engine_command;
+
+typedef enum _engine_answers {
+	UCI_OK,
+	READY_OK,
+	NORMAL_ANSW,
+	NO_ANSW
+} engine_answers;
+
 typedef struct _engine
 {
 	LPCWSTR path_to_engine;         // Full path to engine.exe
@@ -20,11 +45,22 @@ typedef struct _engine
 } engine;
 
 
-/* Initialize all fields with NULL and copy path to field path_to_engine[] */
+/* Initialize all fields with NULL and copy path to field path_to_engine[]. */
 void init_engine_struct(engine*, LPCWSTR path_to_engine);
 
-/* Call after init_engine_struct. Create new thread and 2 pipes for communication. Run chess_engine.exe */
+/* Call after init_engine_struct. Create new thread and 2 pipes for communication. Run chess_engine.exe. */
 engine_error engine_load(engine* ptr_to_engine);
 
 /* Closes engine. */
 void engine_close(engine* ptr_to_engine);
+
+/* Send a fixed specified command to engine process. */
+void engine_send_command(engine*, engine_command_type, engine_command, char* buff_answ);
+
+/* Set options for engine and send them to engine process. */
+void engine_set_options();
+
+/* Look at answer and return one of the engine_answers*/
+engine_answers engine_identify_answer(char* answ);
+
+
