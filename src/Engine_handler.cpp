4f9d6fbeac542e328f2d585dedd0a958775080a2
ZltZZZ <93077810+ZltZZZ@users.noreply.github.com>
2022-11-14 07:05:57 +0300

    Add new send_command func, output engine parser func.

--- src/Engine_handler.cpp
+++ src/Engine_handler.cpp
@@ -1,5 +1,6 @@
 #include "Engine_handler.h"
 #include <string.h>
+#include <string>
 
 void init_engine_struct(engine* engn, LPCWSTR path_to_engine) {
 	engn->path_to_engine = path_to_engine;
@@ -9,7 +10,7 @@ void init_engine_struct(engine* engn, LPCWSTR path_to_engine) {
 }
 
 engine_error engine_load(engine* engn) {
-	// Creates an engine process (run engine.exe)
+	// Creates an engine process (run engine.exe).
 	if (create_process(engn->path_to_engine, &engn->pipe_in_w, &engn->pipe_out_r) == PROCESS_CREATE_OK) {
 		char answ[MAX_MSG_SIZE] = { '\0' };
 
@@ -17,11 +18,17 @@ engine_error engine_load(engine* engn) {
 		wait_for_answ(&engn->pipe_out_r);
 		clean_pipe_out(&engn->pipe_out_r);
 
-		// Send command "uci" as specificated in uci protocol docs
+		// Send command "uci" as specificated in uci protocol docs.
 		engine_send_command(engn, COMMAND_TYPE_GET_ANSW, UCI, answ);
 		if (engine_identify_answer(answ) != UCI_OK) {
 			return ENGINE_LOAD_FAIL;
 		}
+		
+		// Send command "ucinewgame" to finally init engine.
+		engine_send_command(engn, COMMAND_TYPE_PASSTHROUGH, UCI_NEW_GAME, NULL);
+
+		// Send command "setoption" to setup params of engine.
+		engine_set_def_options(engn);
 
 		engn->state = WORKING;
 		return ENGINE_LOAD_OK;
@@ -38,7 +45,6 @@ void engine_close(engine* engn) {
 	engn->state = NOT_LOADED;
 }
 
-/* Send command to engine. If command type set as COMMAND_TYPE_PASSTHROUGH, than buff_answ should be set as NULL*/
 void engine_send_command(engine* engn, engine_command_type command_type, engine_command command, char* buff_answ) {
 	char msg[MAX_MSG_SIZE] = { '\0' };
 
@@ -48,21 +54,16 @@ void engine_send_command(engine* engn, engine_command_type command_type, engine_
 		memcpy(msg, "uci\n", sizeof(char) * 5);
 		break;
 	case DEBUG_ON:
+		memcpy(msg, "debug on\n", sizeof(char) * 10);
 		break;
 	case DEBUG_OFF:
+		memcpy(msg, "debug off\n", sizeof(char) * 11);
 		break;
 	case IS_READY:
 		memcpy(msg, "isready\n", sizeof(char) * 9);
 		break;
-	case SET_OPTION:
-		break;
 	case UCI_NEW_GAME:
 		memcpy(msg, "ucinewgame\n", sizeof(char) * 12);
-		break;
-	case DO_NEXT_MOVE:
-		break;
-	case START_ANALYZE:
-		memcpy(msg, "go\n", sizeof(char) * 4);
 		break;
 	case STOP_ANALYZE:
 		memcpy(msg, "stop\n", sizeof(char) * 6);
@@ -89,8 +90,108 @@ void engine_send_command(engine* engn, engine_command_type command_type, engine_
 	}
 }
 
-void engine_set_options() {
+void engine_send_command_custom(engine* engn, engine_command_type command_type, char* msg, char* buff_answ) {
+	switch (command_type)
+	{
+	case COMMAND_TYPE_PASSTHROUGH:
+		send_message(&engn->pipe_in_w, msg);
+		break;
+	case COMMAND_TYPE_GET_ANSW:
+		send_message(&engn->pipe_in_w, msg);
+		wait_for_answ(&engn->pipe_out_r);
+		recieve_message(&engn->pipe_out_r, buff_answ);
+		break;
+	default:
+		break;
+	}
+}
 
+void engine_send_command_start_analyze(engine* engn, int move_time) {
+	std::string buff;
+
+	buff = "go movetime ";
+	buff += std::to_string(move_time * 1000);
+	buff += "\n";
+
+	engine_send_command_custom(engn, COMMAND_TYPE_PASSTHROUGH, (char*)buff.c_str(), NULL);
+}
+
+void engine_set_def_options(engine* engn) {
+	std::string buff;
+
+	buff = "setoption name MultiPV value ";
+	buff += std::to_string(OPTION_MULTI_PV_DEF);
+	buff += "\n";
+
+	engine_send_command_custom(engn, COMMAND_TYPE_PASSTHROUGH, (char*)buff.c_str(), NULL);
+}
+
+void engine_set_position(engine* engn, char* fen_string) {
+	std::string buff;
+	char answ[MAX_MSG_SIZE];
+
+	buff = "position fen ";
+	buff += fen_string;
+	buff += "\n";
+
+	engine_send_command_custom(engn, COMMAND_TYPE_PASSTHROUGH, (char*)buff.c_str(), NULL);
+
+	// Send "isready" command, so wait until answers "readyok".
+	engine_send_command(engn, COMMAND_TYPE_GET_ANSW, IS_READY, answ);
+}
+
+void engine_parse_analisys_output(engine* engn, engine_line* line) {
+	char word[MAX_WORD_SIZE] = { '\0' };
+	int word_indx = 0, // Index of word string
+		line_indx = 0; // Index of line
+	char c_buff;
+	bool is_word = false,  // Word flag
+		 is_line = false,  // If word == multipv, than flag true
+		 is_move = false;  // If word == pv, than flag true
+
+	// Continue get data until receive "bestmove" word from engine
+	while (memcmp(word, "bestmove", 9) != 0)
+	{
+		wait_for_answ(&engn->pipe_out_r);
+		recieve_char(&engn->pipe_out_r, &c_buff);
+
+		if (c_buff == ' ' || c_buff == '\n') { // null-characters zero word string
+			if (is_word) {
+				if (is_line) {
+					line_indx = atoi(word); // Remember index of line
+					is_line = false;
+				}
+				else if (is_move) { // Remember move from remembered line
+					memcpy(line[line_indx - 1].move, word, sizeof(char) * MAX_MOVE_SIZE);
+					is_move = false;
+				}
+
+				is_word = false;
+				word_indx = 0;
+				memset(word, '\0', sizeof(char) * MAX_WORD_SIZE);
+			}
+		}
+		else
+		{
+			if (is_word == false) is_word = true;
+
+			// Gather word
+			word[word_indx++] = c_buff;
+
+			if (memcmp(word, "multipv", 8) == 0) {
+				is_line = true;
+				is_word = false; // This is a little crutch to avoid logical error.
+				word_indx = 0;
+				memset(word, '\0', sizeof(char) * MAX_WORD_SIZE);
+			}
+			else if (memcmp(word, "pv", 3) == 0) {
+				is_move = true;
+				is_word = false; // The same as upper.
+				word_indx = 0;
+				memset(word, '\0', sizeof(char) * MAX_WORD_SIZE);
+			}
+		}
+	}
 }
 
 engine_answers engine_identify_answer(char* answ) {
--- src/Engine_handler.h
+++ src/Engine_handler.h
@@ -1,3 +1,9 @@
+#pragma once
+
+#define OPTION_MULTI_PV_DEF 4 // Default count of lines to analysis
+#define MAX_MOVE_SIZE 10
+#define MAX_WORD_SIZE 100
+
 #include "Process_handler.h"
 
 enum _engine_state {
@@ -21,10 +27,7 @@ typedef enum _engine_command {
 	DEBUG_ON,
 	DEBUG_OFF,
 	IS_READY,
-	SET_OPTION,
 	UCI_NEW_GAME,
-	DO_NEXT_MOVE,
-	START_ANALYZE,
 	STOP_ANALYZE,
 	QUIT_ENGINE
 } engine_command;
@@ -44,6 +47,9 @@ typedef struct _engine
 	enum _engine_state state;       // Current state of engine (working or not and so on)
 } engine;
 
+typedef struct _engine_line {
+	char move[MAX_MOVE_SIZE];
+} engine_line;
 
 /* Initialize all fields with NULL and copy path to field path_to_engine[]. */
 void init_engine_struct(engine*, LPCWSTR path_to_engine);
@@ -54,11 +60,23 @@ engine_error engine_load(engine* ptr_to_engine);
 /* Closes engine. */
 void engine_close(engine* ptr_to_engine);
 
-/* Send a fixed specified command to engine process. */
+/* Send a simple command to engine. If command type set as COMMAND_TYPE_PASSTHROUGH, than buff_answ should be set as NULL. */
 void engine_send_command(engine*, engine_command_type, engine_command, char* buff_answ);
 
-/* Set options for engine and send them to engine process. */
-void engine_set_options();
+/* Send a custom message to engine. */
+void engine_send_command_custom(engine*, engine_command_type, char* msg, char* buff_answ);
+
+/* Send to engine command "go", that force to start thinking. End of analyze after move_time seconds. */
+void engine_send_command_start_analyze(engine*, int move_time);
+
+/* Set default options for engine and send them to engine process. */
+void engine_set_def_options(engine*);   // Number of lines with analisys
+
+/* Set internal position in engine to analisys, in FEN notation. */
+void engine_set_position(engine*, char* fen_string);
+
+/* Recieve all output from engine when it is analising, extract from that input first move of each line.*/
+void engine_parse_analisys_output(engine*, engine_line*);
 
 /* Look at answer and return one of the engine_answers*/
 engine_answers engine_identify_answer(char* answ);
