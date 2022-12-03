b97e9af2538769ebe43c6f15e3bcb286edd1a699
ZltZZZ <dudalevadudaleva@mail.ru>
2022-12-02 20:29:31 -0500

    Prepare enviroment.

--- qt/Tool_Gui/mainwindow.cpp
+++ qt/Tool_Gui/mainwindow.cpp
@@ -1,5 +1,5 @@
 #include "mainwindow.h"
-#include "ui_mainwindow.h"
+#include "./ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -16,6 +16,6 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_actionQuit_triggered()
 {
-
+    close();
 }
 
